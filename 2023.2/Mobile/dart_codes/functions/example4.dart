2ed7bfb5805b1c332fb2c7bfa8bb6af957c6ecca
Margoni Thiago (CaP/ETS) <109745013+ThiagoMargoni@users.noreply.github.com>
2023-09-06 21:24:57 -0300

    Update example4.dart

--- 2023.2/Mobile/dart_codes/functions/example4.dart
+++ 2023.2/Mobile/dart_codes/functions/example4.dart
@@ -3,10 +3,21 @@ void calc_salary(double salary, Function f) {
   f();
 }
 
+double calc_salary2(double salary, double desc, Function f) {
+  f();
+  return salary -= salary * desc;
+}
+
+void show_message() {
+  print('Payment day ');
+}
+
 void calc_bonus() {
   print('Bonus R\$ 50.0');
 }
 
 void main() {
-  calc_salary(5000, calc_bonus);
+  // calc_salary(5000, calc_bonus);
+  double salary = calc_salary2(500, 0.1, show_message);
+  print('Salary: $salary');
 }
